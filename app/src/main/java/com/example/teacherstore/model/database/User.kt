440ca3e820be38c895561658cd8913b0bcfe9be9
CattoElectrico <lucas8422olmedoj@gmail.com>
2025-11-02 18:42:47 -0300

    vuelvo en 2

---
+++ app/src/main/java/com/example/teacherstore/model/database/User.kt
@@ -0,0 +1,14 @@
+package com.example.teacherstore.model.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+class User (
+){
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0;
+    val name: String = "";
+    val email: String = "";
+    val password: String = "";
+}
